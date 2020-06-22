6800cbe7c7b1201ce97df76e724a8ab083c97cad
eapple <softwrengr@gmail.com>
2020-06-23 00:18:25 +0400

    payment screen updated

--- app/src/main/java/com/aldar/studentportal/ui/activities/common/WebActivity.java
+++ app/src/main/java/com/aldar/studentportal/ui/activities/common/WebActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -14,21 +15,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aldar.studentportal.R;
-import com.aldar.studentportal.models.contactsModel.ContactDataModel;
-import com.aldar.studentportal.worker.WebClient;
 
-import java.util.ArrayList;
-
+import io.fabric.sdk.android.services.network.NetworkUtils;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.MaybeEmitter;
 import io.reactivex.rxjava3.core.MaybeObserver;
 import io.reactivex.rxjava3.core.MaybeOnSubscribe;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Supplier;
-import io.reactivex.rxjava3.observers.DisposableObserver;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class WebActivity extends AppCompatActivity {
@@ -109,40 +103,42 @@ public class WebActivity extends AppCompatActivity {
         webView.getSettings().setBuiltInZoomControls(false);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
         webView.setWebViewClient(new HelloWebViewClient());
         webView.loadUrl(link);
+
+        if (!com.aldar.studentportal.utilities.NetworkUtils.isNetworkConnected(this)) {
+            webView.setVisibility(View.GONE);
+            tvNoAccess.setVisibility(View.VISIBLE);
+        }
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+
+            }
+        });
     }
 
     private class HelloWebViewClient extends WebViewClient {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            loadingFinished = false;
             progressBar.setVisibility(View.VISIBLE);
             super.onPageStarted(view, url, favicon);
         }
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String newUrl) {
-            if (!loadingFinished) {
-                redirect = true;
+            if (!com.aldar.studentportal.utilities.NetworkUtils.isNetworkConnected(WebActivity.this)) {
+                webView.setVisibility(View.GONE);
+                tvNoAccess.setVisibility(View.VISIBLE);
             }
 
-            loadingFinished = false;
             webView.loadUrl(newUrl);
             return true;
         }
 
         @Override
         public void onPageFinished(WebView view, String url) {
-            if (!redirect) {
-                loadingFinished = true;
-                progressBar.setVisibility(View.GONE);
-                progressBar = null;
-            } else {
-                webView.setVisibility(View.GONE);
-                redirect = false;
-            }
+            progressBar.setVisibility(View.GONE);
         }
     }
 
